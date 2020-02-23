afa84e084fe5bb3bc229822390202548cdee1d1a
Yi Zhang <zhangyiant@hotmail.com>
2020-02-23 17:05:00 +0800

    Remove unused fake data.

--- src/app/register-printer.service.ts
+++ src/app/register-printer.service.ts
@@ -15,50 +15,15 @@ import { RegisterPrinterDoc } from './register-printer-doc';
 })
 export class RegisterPrinterService {
 
-  topSys: TopSys = new TopSys('Top_Module');
+  topSys: TopSys;
 
   documentOpenedSource = new Subject<TopSys>();
   documentOpened$ = this.documentOpenedSource.asObservable();
 
-  private registerPrinterDocsUrl = "register-printer/api/register-printer-docs/";
+  private registerPrinterDocsUrl = "register-printer/api/register-printer-docs";
 
   constructor(
     private http: HttpClient) {
-    this.topSys.version = '2.1.234';
-    this.topSys.author = 'Peter';
-    this.topSys.addressWidth = 12;
-    this.topSys.dataWidth = 32;
-    let blockType: BlockType;
-    let register: Register;
-    let field: Field;
-    blockType = new BlockType('Block Type1', 65536, 12, 32);
-    this.topSys.addBlockType(blockType);
-    register = new Register('reg1', 0);
-    field = new Field('Field1', 7, 1, 1, 'RW');
-    register.addField(field);
-    field = new Field('Field2', 10, 8, 1, 'RW');
-    register.addField(field);
-    blockType.addRegister(register);
-    register = new Register('reg2', 2);
-    field = new Field('Field2', 7, 1, 1, 'RW');
-    register.addField(field);
-    field = new Field('Field3', 12, 9, 1, 'RW');
-    register.addField(field);
-    blockType.addRegister(register);
-    blockType = new BlockType('Block Type2', 16, 24, 64);
-    this.topSys.addBlockType(blockType);
-    register = new Register('reg1', 0);
-    field = new Field('Field1', 7, 1, 1, 'RW');
-    register.addField(field);
-    field = new Field('Field2', 10, 8, 1, 'RW');
-    register.addField(field);
-    blockType.addRegister(register);
-    register = new Register('reg2', 1);
-    field = new Field('Field2', 7, 1, 1, 'RW');
-    register.addField(field);
-    field = new Field('Field3', 12, 9, 1, 'RW');
-    register.addField(field);
-    blockType.addRegister(register);
     return;
   }
 
@@ -72,7 +37,6 @@ export class RegisterPrinterService {
   }
 
   parseDoc(strDoc: string): TopSys | null {
-    console.log("parseDoc called");
     console.log(strDoc);
     const jsonObj: object = JSON.parse(strDoc);
     const topSys = TopSys.parseJson(jsonObj);
