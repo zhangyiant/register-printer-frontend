3e46a9bd2fe9218220605f83f1e41c7937d8b4d0
Yi Zhang <zhangyiant@hotmail.com>
2020-03-26 19:59:10 +0800

    Add electron startup script.

---
+++ index.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.webContents.openDevTools();
+
+  win.loadFile('dist/register-printer-frontend/index.html');
+}
+
+app.whenReady().then(createWindow);
