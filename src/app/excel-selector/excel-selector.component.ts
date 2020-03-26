e0f331bf59bd570d5c147ac53bc9ab0f9f0e4253
Yi Zhang <zhangyiant@hotmail.com>
2020-03-26 21:31:41 +0800

    Add input path.

--- src/app/excel-selector/excel-selector.component.ts
+++ src/app/excel-selector/excel-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { remote } from 'electron';
 
 @Component({
   selector: 'app-excel-selector',
@@ -7,9 +8,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExcelSelectorComponent implements OnInit {
 
+  configFile: string;
+  excelPath: string;
+  outputPath: string;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onConfigFileClicked() {
+    const { dialog } = remote;
+    const currentWindow = remote.getCurrentWindow();
+    dialog.showOpenDialog(
+      currentWindow,
+      {
+        properties: [
+          'openFile'
+        ],
+        filters: [
+          {
+            name: 'Excel files',
+            extensions: ['xlsx']
+          }
+        ]
+      }
+    ).then(
+      (result) => {
+        if (!result.canceled) {
+          this.configFile = result.filePaths[0];
+        }
+      }
+    );
+    return;
+  }
+
+  onExcelPathClicked() {
+    const { dialog } = remote;
+    const currentWindow = remote.getCurrentWindow();
+    dialog.showOpenDialog(
+      currentWindow,
+      {
+        properties: [
+          'openDirectory'
+        ]
+      }
+    ).then(
+      (result) => {
+        if (!result.canceled) {
+          this.excelPath = result.filePaths[0];
+        }
+      }
+    );
+    return;
+  }
+
+  onOutputPathClicked() {
+    const { dialog } = remote;
+    const currentWindow = remote.getCurrentWindow();
+    dialog.showOpenDialog(
+      currentWindow,
+      {
+        properties: [
+          'openDirectory'
+        ]
+      }
+    ).then(
+      (result) => {
+        if (!result.canceled) {
+          this.outputPath = result.filePaths[0];
+        }
+      }
+    );
+    return;
+  }
 }
