7c3d66980c2872215073912f97fc7283d9238bc4
Yi Zhang <zhangyiant@hotmail.com>
2020-10-03 15:50:07 +0800

    Add inherited-number-edit-box.

--- src/app/ui-components/inherited-number-edit-box/inherited-number-edit-box.component.ts
+++ src/app/ui-components/inherited-number-edit-box/inherited-number-edit-box.component.ts
@@ -12,9 +12,57 @@ export class InheritedNumberEditBoxComponent implements OnInit {
 
   @Output() valueChange = new EventEmitter<number>();
 
+  editingValue: string;
+  editingIsInherited: boolean;
+  isEditing = false;
+
   constructor() { }
+
+  get displayValue() {
+    if (this.value) {
+      return this.value.toString();
+    } else {
+      return this.inheritedValue.toString() + "(inherited)";
+    }
+  }
 
   ngOnInit(): void {
   }
 
+  onDoubleClick(event) {
+    this.isEditing = true;
+    if (this.value === null) {
+      this.editingIsInherited = true;
+      this.editingValue = this.inheritedValue.toString() + "(inherited)";
+    } else {
+      this.editingValue = this.value.toString();
+      this.editingIsInherited = false;
+    }
+    return;
+  }
+
+  onSubmit(event) {
+    if (this.editingIsInherited) {
+      this.isEditing = false;
+      this.valueChange.emit(null);
+    } else {
+      this.value = parseInt(this.editingValue);
+      this.isEditing = false;
+      this.valueChange.emit(this.value);
+    }
+    return;
+  }
+
+  onCancel(event) {
+    this.isEditing = false;
+    return;
+  }
+
+  onCheckBoxChange() {
+    if (this.editingIsInherited) {
+      this.editingValue = this.inheritedValue.toString() + "(inherited)";
+    } else {
+      this.editingValue = this.inheritedValue.toString();
+    }
+  }
 }
