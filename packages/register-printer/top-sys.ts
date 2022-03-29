7b6ce36d6e08552ed67052cee838ab0f600d3a2d
Yi Zhang <zhangyiant@hotmail.com>
2022-03-29 20:14:21 +0800

    Format adjustment.

--- packages/register-printer/top-sys.ts
+++ packages/register-printer/top-sys.ts
@@ -1,25 +1,25 @@
-import { BlockTemplate } from './block-template';
-import { BlockInstance } from './block-instance';
+import {BlockTemplate} from './block-template';
+import {BlockInstance} from './block-instance';
 
 export class TopSys {
-    name: string;
-    addressWidth: number;
-    dataWidth: number;
-    version: string | null;
-    author: string | null;
-    blockTemplates: Array<BlockTemplate>;
-    blockInstances: Array<BlockInstance>;
+  name: string;
+  addressWidth: number;
+  dataWidth: number;
+  version: string | null;
+  author: string | null;
+  blockTemplates: Array<BlockTemplate>;
+  blockInstances: Array<BlockInstance>;
 
-    constructor(name: string, addressWidth: number, dataWidth: number) {
-        this.name = name;
-        this.addressWidth = addressWidth;
-        this.dataWidth = dataWidth;
-        this.version = null;
-        this.author = null;
-        this.blockTemplates = [];
-        this.blockInstances = [];
-        return;
-    }
+  constructor(name: string, addressWidth: number, dataWidth: number) {
+    this.name = name;
+    this.addressWidth = addressWidth;
+    this.dataWidth = dataWidth;
+    this.version = null;
+    this.author = null;
+    this.blockTemplates = [];
+    this.blockInstances = [];
+    return;
+  }
 
   addBlockTemplate(blockTemplate: BlockTemplate) {
     this.blockTemplates.push(blockTemplate);
@@ -33,31 +33,31 @@ export class TopSys {
 
   toJson(): object {
     const jsonObj: object = {};
-    jsonObj['name'] = this.name;
-    jsonObj["addressWidth"] = this.addressWidth;
-    jsonObj["dataWidth"] = this.dataWidth;
-    jsonObj["version"] = this.version;
-    jsonObj["author"] = this.author;
-    jsonObj["blockTemplates"] = [];
+    jsonObj["name"] = this.name;
+    jsonObj['addressWidth'] = this.addressWidth;
+    jsonObj['dataWidth'] = this.dataWidth;
+    jsonObj['version'] = this.version;
+    jsonObj['author'] = this.author;
+    jsonObj['blockTemplates'] = [];
     for (let blockType of this.blockTemplates) {
       const blockTypeJsonObj = blockType.toJson();
-      jsonObj["blockTemplates"].push(blockTypeJsonObj);
+      jsonObj['blockTemplates'].push(blockTypeJsonObj);
     }
-    jsonObj["blockInstances"] = [];
+    jsonObj['blockInstances'] = [];
     for (let addressMap of this.blockInstances) {
       const addressMapJsonObj = addressMap.toJson();
-      jsonObj["blockInstances"].push(addressMapJsonObj);
+      jsonObj['blockInstances'].push(addressMapJsonObj);
     }
     return jsonObj;
   }
 
   static parseJson(jsonObj: object): TopSys {
     const topSys = new TopSys(
-      jsonObj["name"],
-      jsonObj["addressWidth"],
-      jsonObj["dataWidth"]);
-    topSys.version = jsonObj["version"];
-    topSys.author = jsonObj["author"];
+      jsonObj['name'],
+      jsonObj['addressWidth'],
+      jsonObj['dataWidth']);
+    topSys.version = jsonObj['version'];
+    topSys.author = jsonObj['author'];
     const blockTemplatesJsonObj = jsonObj['blockTemplates'];
     for (const blockTemplateJsonObj of blockTemplatesJsonObj) {
       const blockTemplate = BlockTemplate.parseJson(
@@ -65,7 +65,7 @@ export class TopSys {
       );
       topSys.addBlockTemplate(blockTemplate);
     }
-    const blockInstancesJsonObj = jsonObj["blockInstances"];
+    const blockInstancesJsonObj = jsonObj['blockInstances'];
     for (const blockInstanceJsonObj of blockInstancesJsonObj) {
       const blockInstance = BlockInstance.parseJson(
         blockInstanceJsonObj);
