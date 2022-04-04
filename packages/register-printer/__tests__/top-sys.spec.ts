5b09ecf5685a0fcc4bae5119d20bbdb9cae9e7e9
Yi Zhang <zhangyiant@hotmail.com>
2022-04-04 10:07:52 +0800

    Update the TopSys creation test case.

--- packages/register-printer/__tests__/top-sys.spec.ts
+++ packages/register-printer/__tests__/top-sys.spec.ts
@@ -2,6 +2,14 @@ import { TopSys } from '../top-sys';
 
 describe('TopSys', () => {
   test('should create an instance', () => {
-    expect(new TopSys('TopSys1', 32, 32)).toBeTruthy();
+    const topSys: TopSys = new TopSys('TopSys1', 24, 32);
+    expect(topSys).toBeTruthy();
+    expect(topSys.name).toBe('TopSys1');
+    expect(topSys.addressWidth).toBe(24);
+    expect(topSys.dataWidth).toBe(32);
+    expect(topSys.version).toBeNull();
+    expect(topSys.author).toBeNull();
+    expect(topSys.blockTemplates).toStrictEqual([]);
+    expect(topSys.blockInstances).toStrictEqual([]);
   });
 });
