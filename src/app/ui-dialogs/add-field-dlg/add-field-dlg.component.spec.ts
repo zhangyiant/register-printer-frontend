bd5602b8042e86353a88a924d94b93548870ccfa
Yi Zhang <zhangyiant@hotmail.com>
2022-04-09 18:15:37 +0800

    Fixed AddFieldDlgComponent test case.

--- src/app/ui-dialogs/add-field-dlg/add-field-dlg.component.spec.ts
+++ src/app/ui-dialogs/add-field-dlg/add-field-dlg.component.spec.ts
@@ -1,6 +1,21 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddFieldDlgComponent } from './add-field-dlg.component';
+import { MatDialogRef } from '@angular/material/dialog'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { NumberEditBoxComponent } from '../../ui-components/number-edit-box/number-edit-box.component';
+import { HexEditBoxComponent } from '../../ui-components/hex-editor-box/hex-edit-box.component';
+import { AccessSelectorComponent } from '../../ui-components/access-selector/access-selector.component';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+class MockMatDialogRef {
+  close(): void {
+  }
+}
 
 describe('AddFieldDlgComponent', () => {
   let component: AddFieldDlgComponent;
@@ -8,7 +23,20 @@ describe('AddFieldDlgComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddFieldDlgComponent ]
+      declarations: [ AddFieldDlgComponent, NumberEditBoxComponent, HexEditBoxComponent, AccessSelectorComponent],
+      imports: [
+        FormsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatDialogModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        {
+          provide: MatDialogRef, useClass: MockMatDialogRef
+        }
+      ]
     })
     .compileComponents();
   }));
