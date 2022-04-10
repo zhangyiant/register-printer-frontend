39310a0e931427497c3123f4b9099aa27edec3ec
Yi Zhang <zhangyiant@hotmail.com>
2022-04-10 19:12:23 +0800

    Format adjustment.

--- packages/register-printer/src/block-template.ts
+++ packages/register-printer/src/block-template.ts
@@ -1,13 +1,27 @@
 import { RegisterTemplate } from './register-template';
 
 export class BlockTemplate {
-  blockType: string;
-  registerTemplates: Array<RegisterTemplate>;
 
   constructor(blockType: string) {
     this.blockType = blockType;
     this.registerTemplates = [];
     return;
+  }
+  blockType: string;
+  registerTemplates: Array<RegisterTemplate>;
+
+  static parseJson(jsonObj: any): BlockTemplate {
+    const blockTemplate = new BlockTemplate(
+      jsonObj.blockType
+    );
+
+    const registersJsonObj = jsonObj.registerTemplates;
+    for (const registerJsonObj of registersJsonObj) {
+      const register = RegisterTemplate.parseJson(
+        registerJsonObj);
+      blockTemplate.addRegister(register);
+    }
+    return blockTemplate;
   }
 
   addRegister(register: RegisterTemplate) {
@@ -17,26 +31,12 @@ export class BlockTemplate {
 
   toJson(): object {
     const jsonObj: any = {};
-    jsonObj["blockType"] = this.blockType;
-    jsonObj["registerTemplates"] = [];
+    jsonObj.blockType = this.blockType;
+    jsonObj.registerTemplates = [];
     for (const register of this.registerTemplates) {
       const registerJsonObj = register.toJson();
-      jsonObj['registerTemplates'].push(registerJsonObj);
+      jsonObj.registerTemplates.push(registerJsonObj);
     }
     return jsonObj;
-  }
-
-  static parseJson(jsonObj: any): BlockTemplate {
-    const blockTemplate = new BlockTemplate(
-      jsonObj["blockType"]
-    );
-
-    const registersJsonObj = jsonObj["registerTemplates"];
-    for (const registerJsonObj of registersJsonObj) {
-      const register = RegisterTemplate.parseJson(
-        registerJsonObj);
-      blockTemplate.addRegister(register);
-    }
-    return blockTemplate;
   }
 }
