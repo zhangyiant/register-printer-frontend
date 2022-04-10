98962837fa3e64f079d4d282daef1416997bd385
Yi Zhang <zhangyiant@hotmail.com>
2022-04-10 15:46:07 +0800

    Turn on devtools.

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 const { app, Menu, dialog, ipcMain, BrowserWindow } = require('electron');
-import * as path from "path";
+import * as path from 'path';
 const { autoUpdater } = require('electron-updater');
 const { getRegisterPrinterVersion } = require('./get-register-printer-version');
 
 
 function createWindow() {
   const preloadScriptPath = path.join(__dirname, 'preload.js');
-  let win = new BrowserWindow({
+  const win = new BrowserWindow({
     width: 1200,
     height: 900,
     webPreferences: {
@@ -16,15 +16,15 @@ function createWindow() {
     }
   });
 
-  ipcMain.on("get-app-path", (event) => {
+  ipcMain.on('get-app-path', (event) => {
     event.returnValue = app.getAppPath();
   });
 
-  ipcMain.on("get-version", (event) => {
+  ipcMain.on('get-version', (event) => {
     event.returnValue = app.getVersion();
   });
 
-  ipcMain.on("open-export-to-excel-dialog", (event) => {
+  ipcMain.on('open-export-to-excel-dialog', (event) => {
     dialog.showOpenDialog(
       win,
       {
@@ -34,12 +34,12 @@ function createWindow() {
       }
     ).then(
       (result) => {
-        event.reply("open-export-to-excel-dialog-reply", result);
+        event.reply('open-export-to-excel-dialog-reply', result);
       }
     );
   });
 
-  ipcMain.on("open-export-to-json-dialog", (event) => {
+  ipcMain.on('open-export-to-json-dialog', (event) => {
     dialog.showSaveDialog(
       win,
       {
@@ -53,12 +53,12 @@ function createWindow() {
       }
     ).then(
       (result) => {
-        event.reply("open-export-to-json-dialog-reply", result);
+        event.reply('open-export-to-json-dialog-reply', result);
       }
     );
   });
 
-  ipcMain.on("open-load-from-json-dialog", (event) => {
+  ipcMain.on('open-load-from-json-dialog', (event) => {
     dialog.showOpenDialog(
       win,
       {
@@ -74,12 +74,12 @@ function createWindow() {
       }
     ).then(
       (result) => {
-        event.reply("open-load-from-json-dialog-reply", result);
+        event.reply('open-load-from-json-dialog-reply', result);
       }
     );
   });
 
-  ipcMain.on("open-generate-dialog", (event) => {
+  ipcMain.on('open-generate-dialog', (event) => {
     dialog.showOpenDialog(
       win,
       {
@@ -89,12 +89,12 @@ function createWindow() {
       }
     ).then(
       (result) => {
-        event.reply("open-generate-dialog-reply", result);
+        event.reply('open-generate-dialog-reply', result);
       }
     );
   });
 
-  ipcMain.on("open-config-file-dialog", (event) => {
+  ipcMain.on('open-config-file-dialog', (event) => {
     dialog.showOpenDialog(
       win,
       {
@@ -110,12 +110,12 @@ function createWindow() {
       }
     ).then(
       (result) => {
-        event.reply("open-config-file-dialog-reply", result);
+        event.reply('open-config-file-dialog-reply', result);
       }
     );
   });
 
-  ipcMain.on("open-excel-path-dialog", (event) => {
+  ipcMain.on('open-excel-path-dialog', (event) => {
     dialog.showOpenDialog(
       win,
       {
@@ -125,12 +125,12 @@ function createWindow() {
       }
     ).then(
       (result) => {
-        event.reply("open-excel-path-dialog-reply", result);
+        event.reply('open-excel-path-dialog-reply', result);
       }
     );
   });
 
-  ipcMain.on("open-output-path-dialog", (event) => {
+  ipcMain.on('open-output-path-dialog', (event) => {
     dialog.showOpenDialog(
       win,
       {
@@ -140,30 +140,30 @@ function createWindow() {
       }
     ).then(
       (result) => {
-        event.reply("open-output-path-dialog-reply", result);
+        event.reply('open-output-path-dialog-reply', result);
       }
     );
   });
 
-  // win.webContents.openDevTools();
+  win.webContents.openDevTools();
 
   win.loadFile('dist/register-printer-frontend/index.html');
 }
 
 const template = [
   {
-    label: "About",
+    label: 'About',
     submenu: [
       {
-        label: "About RegisterPrinter",
+        label: 'About RegisterPrinter',
         click: () => {
           dialog.showMessageBox(
             {
-              type: "info",
-              title: "About RegisterPrinter",
-              message: "Version: " + app.getVersion() + "\n" +
-                "RegisterPrinter Version: " + getRegisterPrinterVersion() + "\n" +
-                "Electronjs Version: " + process.versions.electron
+              type: 'info',
+              title: 'About RegisterPrinter',
+              message: 'Version: ' + app.getVersion() + '\n' +
+                'RegisterPrinter Version: ' + getRegisterPrinterVersion() + '\n' +
+                'Electronjs Version: ' + process.versions.electron
             }
           );
         }
@@ -177,6 +177,6 @@ Menu.setApplicationMenu(menu);
 
 app.whenReady().then(createWindow);
 
-autoUpdater.logger = require("electron-log")
-autoUpdater.logger.transports.file.level = "info"
+autoUpdater.logger = require('electron-log');
+autoUpdater.logger.transports.file.level = 'info';
 autoUpdater.checkForUpdatesAndNotify();
