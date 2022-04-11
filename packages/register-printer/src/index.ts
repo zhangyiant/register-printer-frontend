77e8977dec7c0df6ecd23e794c938b8ce5cc4b61
Yi Zhang <zhangyiant@hotmail.com>
2022-04-11 20:09:49 +0800

    Export ArrayTemplate in index.

--- packages/register-printer/src/index.ts
+++ packages/register-printer/src/index.ts
@@ -1,5 +1,6 @@
 export { TopSys } from './top-sys';
 export { BlockInstance } from './block-instance';
 export { BlockTemplate } from './block-template';
+export {ArrayTemplate} from './array-template';
 export { RegisterTemplate } from './register-template';
 export { FieldTemplate, ACCESS_STRINGS } from './field-template';
